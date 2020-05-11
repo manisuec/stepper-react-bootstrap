6e435095fecf6d10ec3968e7ce0b6d2a74c1f65e
Manish Prasad <manishvaishu25@gmail.com>
2020-05-12 01:12:56 +0530

    initial setup

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,30 @@
-import sass from 'rollup-plugin-sass'
-import typescript from 'rollup-plugin-typescript2'
-import serve from 'rollup-plugin-serve'
-import livereload from 'rollup-plugin-livereload'
+import sass from 'rollup-plugin-sass';
+import typescript from 'rollup-plugin-typescript2';
+import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
 
-import pkg from './package.json'
+import pkg from './package.json';
 
 export default {
   input: 'src/index.tsx',
   output: [
     {
       file: pkg.main,
-      format: 'cjs',
+      format: 'esm',
       exports: 'named',
       sourcemap: true,
       strict: false
     }
   ],
-  entry: 'src/index.tsx',
-  dest: 'dist/index.js',
   plugins: [
+    resolve(),
+    commonjs(),
     sass({ insert: true }),
     typescript(),
     serve('dist'),
     livereload('dist')
   ],
   external: ['react', 'react-dom']
-}
+};
